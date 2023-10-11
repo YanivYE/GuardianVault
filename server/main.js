dc7f65ad979e3dd2081676d881f94a9719e02ec2
Elior Yousefi <elioryous@gmail.com>
2023-10-12 01:48:53 +0300

    basic client servo echo messaging

---
+++ server/main.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname));
+
+// Serve the client.html file at a custom route, e.g., '/chat'
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/client.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (message) => {
+    io.emit('message', message); // Broadcast the message to all connected clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Replace 'YOUR_LAN_IP' with the actual LAN IP address of your server machine
+const yourLANIP = '127.0.0.1';
+const port = 8201;
+
+server.listen(port, yourLANIP, () => {
+  console.log(`Server is running on http://${yourLANIP}:${port}`);
+});
