dbbd70f9ee1e8813f43e88c0356c076567f3915e
YanivYE <yanivye106@gmail.com>
2023-10-14 23:55:12 +0300

    added functions and RSA key generate

--- server/server.js
+++ server/server.js
@@ -1,25 +1,33 @@
 LOCAL_IP = '10.100.102.15' // yaniv = 10.100.102.15
+PORT = 8201
 
 
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
 
 const app = express();
 
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(__dirname));
 
-// Serve the client.html file at a custom route, e.g., '/chat'
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, '../client/client.html');
-  res.sendFile(filePath);
-});
+function serveStaticFiles() {
+  app.use(express.static(__dirname));
+}
 
-io.on('connection', (socket) => {
+// Serve the client.html file 
+function serveClientPage() {
+  app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, '../client/client.html');
+    res.sendFile(filePath);
+  });
+}
+
+function handleSocketConnection(socket) {
   console.log('A user connected');
 
   socket.on('message', (message) => {
@@ -33,11 +41,30 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
+}
+
+function generateRSAKeys(){
+  // Generate an RSA key pair for the server
+  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
 });
 
-const yourLANIP = LOCAL_IP;
-const port = 8201;
+// Save the private key securely and provide the public key to the client
+fs.writeFileSync('server-private-key.pem', privateKey, 'utf8');
+const serverPublicKey = publicKey;
+}
 
-server.listen(port, yourLANIP, () => {
-  console.log(`Server is running on http://${yourLANIP}:${port}`);
-});
+
+function startServer() {
+  server.listen(PORT, LOCAL_IP, () => {
+    console.log(`Server is running on http://${LOCAL_IP}:${PORT}`);
+  });
+}
+
+
+serveStaticFiles();
+serveClientPage();
+io.on('connection', handleSocketConnection);
+startServer();
