9bd2547b8236b0998b54d5cd88bd74e627963b5f
Elior Yousefi <elioryous@gmail.com>
2023-10-17 11:52:19 +0300

    chnage title of html and fixed printing message on html error

--- client/client.js
+++ client/client.js
@@ -1,73 +1,3 @@
-/*/ Create an instance of JSEncrypt with the server's public key
-const serverPublicKey = 'YOUR_SERVER_PUBLIC_KEY_HERE'; // Replace with your server's public key
-const encrypt = new JSEncrypt();
-encrypt.setPublicKey(serverPublicKey);
-
-// Function to encrypt a message and send it to the server
-function sendMessage() {
-  const messageInput = document.getElementById('messageInput');
-  const message = messageInput.value;
-
-  // Encrypt the message using the server's public key
-  const encryptedMessage = encrypt.encrypt(message);
-
-  // Send the encrypted message to the server
-  socket.emit('message', encryptedMessage);
-  messageInput.value = '';
-}
-
-// Function to decrypt a received message
-function decryptMessage(encryptedMessage) {
-  // Decrypt the received message using the client's private key (if available)
-  // Note: In a real scenario, the client would not have a private key.
-  // Decryption is typically done on the server.
-  const decryptedMessage = encrypt.decrypt(encryptedMessage);
-  return decryptedMessage;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const socket = io(); // Connect to the Socket.IO server
-
-  const messages = document.getElementById('messages');
-  const messageInput = document.getElementById('messageInput');
-  const sendButton = document.getElementById('sendButton');
-
-  // Listen for incoming messages from the server
-  socket.on('message', (message) => {
-    displayMessage(message);
-  });
-
-  // Handle sending a new message
-  sendButton.addEventListener('click', () => {
-    console.log("click");
-    const message = messageInput.value;
-
-    if (message) {
-      // Send the message to the server
-      socket.emit('message', message);
-
-      // Clear the input field
-      messageInput.value = '';
-    }
-  });
-
-  // Display a received message in the chat
-  function displayMessage(message) {
-    const messageElement = document.createElement('li');
-    messageElement.textContent = message;
-    messages.appendChild(messageElement);
-  }
-});
-
-socket.on('message', (encryptedMessage) => {
-  const decryptedMessage = decryptMessage(encryptedMessage);
-  if (decryptedMessage) {
-    // Display the decrypted message
-    displayMessage(decryptedMessage);
-  }
-});
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
   const socket = io(); // Connect to the Socket.IO server
 
@@ -99,6 +29,9 @@ document.addEventListener('DOMContentLoaded', () => {
       // Send the encrypted message to the server
       socket.emit('message', encryptedMessage);
 
+      // Display the sent message in the chat
+      displaySentMessage(message, encryptedMessage);
+
       // Clear the input field
       messageInput.value = '';
     }
@@ -108,14 +41,23 @@ document.addEventListener('DOMContentLoaded', () => {
   socket.on('message', (encryptedMessage) => {
     const decryptedMessage = decryptMessage(encryptedMessage, encrypt);
     if (decryptedMessage) {
-      // Display the decrypted message
-      displayMessage(decryptedMessage);
+      // Display the received message in the chat
+      displayReceivedMessage(decryptedMessage);
     }
   });
 
-  // Display a received message in the chat
-  function displayMessage(message) {
+  // Function to display a sent message in the chat
+  function displaySentMessage(message, encryptedMessage) {
     const messageElement = document.createElement('li');
+    messageElement.classList.add('sent-message'); // You can add a CSS class for styling
+    messageElement.textContent = "MSG: " + message;
+    messages.appendChild(messageElement);
+  }
+
+  // Function to display a received message in the chat
+  function displayReceivedMessage(message) {
+    const messageElement = document.createElement('li');
+    messageElement.classList.add('received-message'); // You can add a CSS class for styling
     messageElement.textContent = message;
     messages.appendChild(messageElement);
   }
