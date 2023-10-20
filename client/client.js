b24a5e94057adf77edf44e80e4290403e9a9fb30
Elior Yousefi <elioryous@gmail.com>
2023-10-20 12:47:56 +0300

    cannot import crypto help me

--- client/client.js
+++ client/client.js
@@ -1,72 +1,50 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const socket = io(); // Connect to the Socket.IO server
+import crypto from 'crypto';
 
-  const messages = document.getElementById('messages');
-  const messageInput = document.getElementById('messageInput');
-  const sendButton = document.getElementById('sendButton');
-
-  let sharedSecret;
-
-  // Function to display a sent message in the chat
-  function displaySentMessage(message) {
-    const messageElement = document.createElement('li');
-    messageElement.classList.add('sent-message'); // You can add a CSS class for styling
-    messageElement.textContent = 'MSG: ' + message;
-    messages.appendChild(messageElement);
-  }
-
-  // Generate a shared secret using the Web Crypto API
-  async function generateSharedSecret() {
-    try {
-      const randomValues = new Uint8Array(32); // Generate a 256-bit (32-byte) random key
-      crypto.getRandomValues(randomValues);
-      sharedSecret = randomValues;
-    } catch (error) {
-      console.error('Failed to generate a shared secret:', error);
-    }
-  }
-
-  // Function to encrypt a message using the shared secret
-  function encryptWithSharedSecret(sharedSecret, data) {
-    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(sharedSecret), Buffer.from('0123456789abcdef0'));
-    return Buffer.concat([cipher.update(data, 'utf8'), cipher.final()]);
-  }
-
-  // Function to decrypt a message using the shared secret
-  function decryptWithSharedSecret(sharedSecret, data) {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(sharedSecret), Buffer.from('0123456789abcdef0'));
-    return Buffer.concat([decipher.update(data, 'base64', 'utf8'), decipher.final()]);
-  }
-
-  // Handle sending a new message
-  sendButton.addEventListener('click', () => {
-    const message = messageInput.value;
-
-    if (message) {
-      // Encrypt the message using the shared secret
-      const encryptedMessage = encryptWithSharedSecret(sharedSecret, message);
-
-      // Send the encrypted message to the server
-      socket.emit('message', encryptedMessage.toString('base64'));
-
-      // Display the sent message in the chat
-      displaySentMessage(message);
-
-      // Clear the input field
-      messageInput.value = '';
-    }
+function generateKeyPair() {
+  return crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding: { type: 'spki', format: 'pem' },
+    privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
   });
+}
 
-  // Listen for incoming messages from the server
-  socket.on('message', (encryptedMessage) => {
-    // Decrypt the message using the shared secret
-    const decryptedMessage = decryptWithSharedSecret(sharedSecret, Buffer.from(encryptedMessage, 'base64'));
-    if (decryptedMessage) {
-      // Display the received message in the chat
-      displayReceivedMessage(decryptedMessage.toString('utf8'));
-    }
+function performKeyExchange(socket, serverPublicKey) {
+  const dh = crypto.createDiffieHellman(256);
+  dh.generateKeys();
+  socket.emit('exchange-keys', { clientPublicKey: dh.getPublicKey('hex') });
+
+  const sharedSecret = dh.computeSecret(Buffer.from(serverPublicKey, 'base64'));
+  return {
+    dh,
+    sharedSecret,
+  };
+}
+
+function encryptWithSharedSecret(sharedSecret, data) {
+  const cipher = crypto.createCipheriv('aes-256-cbc', sharedSecret, Buffer.from('0123456789abcdef0'));
+  return Buffer.concat([cipher.update(data, 'utf8'), cipher.final()]).toString('base64');
+}
+
+function decryptWithPrivateKey(privateKey, data) {
+  const decryptedData = crypto.privateDecrypt({
+    key: privateKey,
+    padding: crypto.constants.RSA_PKCS1_PADDING,
+  }, Buffer.from(data, 'base64'));
+  return decryptedData.toString('utf8');
+}
+
+const socket = io.connect('http://localhost:8201');
+const clientKeyPair = generateKeyPair();
+
+socket.on('public-key', (serverPublicKey) => {
+  const { dh, sharedSecret } = performKeyExchange(socket, serverPublicKey);
+
+  const data = 'Hello, Server!';
+  const encryptedData = encryptWithSharedSecret(sharedSecret, data);
+  socket.emit('client-message', encryptedData);
+
+  socket.on('server-message', (encryptedResponse) => {
+    const response = decryptWithPrivateKey(clientKeyPair.privateKey, encryptedResponse);
+    console.log('Received and decrypted response:', response);
   });
-
-  // Call this function to generate the shared secret.
-  generateSharedSecret();
 });
