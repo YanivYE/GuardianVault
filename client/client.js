7ff329061313bc790323934577e64fcb689d9ae9
Elior Yousefi <elioryous@gmail.com>
2023-10-20 12:02:21 +0300

    updates client to new encryption like servers

--- client/client.js
+++ client/client.js
@@ -5,37 +5,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const messageInput = document.getElementById('messageInput');
   const sendButton = document.getElementById('sendButton');
 
-  let publicKey;
-  let privateKey;
+  let sharedSecret;
 
-  // Make an AJAX request to fetch the public key from the server
-  fetch('/public-key')
-    .then((response) => response.text())
-    .then((serverPublicKey) => {
-      encrypt = new JSEncrypt();
-      encrypt.setPublicKey(serverPublicKey);
+  // Function to display a sent message in the chat
+  function displaySentMessage(message) {
+    const messageElement = document.createElement('li');
+    messageElement.classList.add('sent-message'); // You can add a CSS class for styling
+    messageElement.textContent = 'MSG: ' + message;
+    messages.appendChild(messageElement);
+  }
 
-      // send the client's public key to the server for future encryption.
-      socket.emit('client-public-key', keyPair.publicKey);
+  // Generate a shared secret using the Web Crypto API
+  async function generateSharedSecret() {
+    try {
+      const randomValues = new Uint8Array(32); // Generate a 256-bit (32-byte) random key
+      crypto.getRandomValues(randomValues);
+      sharedSecret = randomValues;
+    } catch (error) {
+      console.error('Failed to generate a shared secret:', error);
+    }
+  }
 
-      // Set up decryption with the client's private key, assuming the client's private key is already available.
-      decrypt = new JSEncrypt();
-      decrypt.setPrivateKey(keyPair.privateKey);
-    })
-    .catch((error) => {
-      console.error('Failed to fetch the public key:', error);
-    });
+  // Function to encrypt a message using the shared secret
+  function encryptWithSharedSecret(sharedSecret, data) {
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(sharedSecret), Buffer.from('0123456789abcdef0'));
+    return Buffer.concat([cipher.update(data, 'utf8'), cipher.final()]);
+  }
+
+  // Function to decrypt a message using the shared secret
+  function decryptWithSharedSecret(sharedSecret, data) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(sharedSecret), Buffer.from('0123456789abcdef0'));
+    return Buffer.concat([decipher.update(data, 'base64', 'utf8'), decipher.final()]);
+  }
 
   // Handle sending a new message
   sendButton.addEventListener('click', () => {
     const message = messageInput.value;
 
-    if (message && encrypt) {
-      // Encrypt the message using the server's public key
-      const encryptedMessage = encrypt.encrypt(message);
+    if (message) {
+      // Encrypt the message using the shared secret
+      const encryptedMessage = encryptWithSharedSecret(sharedSecret, message);
 
       // Send the encrypted message to the server
-      socket.emit('message', encryptedMessage);
+      socket.emit('message', encryptedMessage.toString('base64'));
 
       // Display the sent message in the chat
       displaySentMessage(message);
@@ -47,38 +59,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Listen for incoming messages from the server
   socket.on('message', (encryptedMessage) => {
-    if (decrypt) {
-      const decryptedMessage = decrypt.decrypt(encryptedMessage);
-      if (decryptedMessage) {
-        // Display the received message in the chat
-        displayReceivedMessage(decryptedMessage);
-      }
+    // Decrypt the message using the shared secret
+    const decryptedMessage = decryptWithSharedSecret(sharedSecret, Buffer.from(encryptedMessage, 'base64'));
+    if (decryptedMessage) {
+      // Display the received message in the chat
+      displayReceivedMessage(decryptedMessage.toString('utf8'));
     }
   });
 
-  // Function to display a sent message in the chat
-  function displaySentMessage(message) {
-    const messageElement = document.createElement('li');
-    messageElement.classList.add('sent-message'); // You can add a CSS class for styling
-    messageElement.textContent = 'MSG: ' + message;
-    messages.appendChild(messageElement);
-  }
-
-  function generateRSAKeyPair() {
-    const keyPair = crypto.generateKeyPairSync('rsa', {
-      modulusLength: 2048,
-      publicKeyEncoding: { type: 'spki', format: 'pem' },
-      privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
-    });
-    publicKey = keyPair.publicKey;
-    privateKey = keyPair.privateKey;
-  }
-
-  // Function to display a received message in the chat
-  function displayReceivedMessage(message) {
-    const messageElement = document.createElement('li');
-    messageElement.classList.add('received-message'); // You can add a CSS class for styling
-    messageElement.textContent = message;
-    messages.appendChild(messageElement);
-  }
+  // Call this function to generate the shared secret.
+  generateSharedSecret();
 });
