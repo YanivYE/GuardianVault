7708ab12f963e7e9d604c16769ca2053306cc3b9
Elior Yousefi <elioryous@gmail.com>
2023-10-20 11:45:43 +0300

    fixed encryption algorithm

--- client/client.js
+++ client/client.js
@@ -5,16 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const messageInput = document.getElementById('messageInput');
   const sendButton = document.getElementById('sendButton');
 
-  let encrypt; // Define the encrypt variable in a scope accessible to both functions
+  let publicKey;
+  let privateKey;
 
   // Make an AJAX request to fetch the public key from the server
   fetch('/public-key')
-    .then(response => response.text())
-    .then(serverPublicKey => {
+    .then((response) => response.text())
+    .then((serverPublicKey) => {
       encrypt = new JSEncrypt();
       encrypt.setPublicKey(serverPublicKey);
+
+      // send the client's public key to the server for future encryption.
+      socket.emit('client-public-key', keyPair.publicKey);
+
+      // Set up decryption with the client's private key, assuming the client's private key is already available.
+      decrypt = new JSEncrypt();
+      decrypt.setPrivateKey(keyPair.privateKey);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error('Failed to fetch the public key:', error);
     });
 
@@ -22,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
   sendButton.addEventListener('click', () => {
     const message = messageInput.value;
 
-    if (message && encrypt) { // Check if encrypt is defined before using it
+    if (message && encrypt) {
       // Encrypt the message using the server's public key
       const encryptedMessage = encrypt.encrypt(message);
 
@@ -30,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
       socket.emit('message', encryptedMessage);
 
       // Display the sent message in the chat
-      displaySentMessage(message, encryptedMessage);
+      displaySentMessage(message);
 
       // Clear the input field
       messageInput.value = '';
@@ -39,19 +47,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Listen for incoming messages from the server
   socket.on('message', (encryptedMessage) => {
-    const decryptedMessage = decryptMessage(encryptedMessage, encrypt);
-    if (decryptedMessage) {
-      // Display the received message in the chat
-      displayReceivedMessage(decryptedMessage);
+    if (decrypt) {
+      const decryptedMessage = decrypt.decrypt(encryptedMessage);
+      if (decryptedMessage) {
+        // Display the received message in the chat
+        displayReceivedMessage(decryptedMessage);
+      }
     }
   });
 
   // Function to display a sent message in the chat
-  function displaySentMessage(message, encryptedMessage) {
+  function displaySentMessage(message) {
     const messageElement = document.createElement('li');
     messageElement.classList.add('sent-message'); // You can add a CSS class for styling
-    messageElement.textContent = "MSG: " + message;
+    messageElement.textContent = 'MSG: ' + message;
     messages.appendChild(messageElement);
+  }
+
+  function generateRSAKeyPair() {
+    const keyPair = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: 'spki', format: 'pem' },
+      privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+    });
+    publicKey = keyPair.publicKey;
+    privateKey = keyPair.privateKey;
   }
 
   // Function to display a received message in the chat
@@ -60,11 +80,5 @@ document.addEventListener('DOMContentLoaded', () => {
     messageElement.classList.add('received-message'); // You can add a CSS class for styling
     messageElement.textContent = message;
     messages.appendChild(messageElement);
-  }
-
-  // Function to decrypt a received message
-  function decryptMessage(encryptedMessage, encrypt) {
-    const decryptedMessage = encrypt.decrypt(encryptedMessage);
-    return decryptedMessage;
   }
 });
