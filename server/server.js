5bda39a6d589d0756f65dfcb5b658a7fd2a2175e
YanivYE <yanivye106@gmail.com>
2023-11-14 21:41:12 +0200

    added server side DH keys exchange

--- server/server.js
+++ server/server.js
@@ -8,20 +8,12 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 const crypto = require('crypto');
-const NodeRSA = require('node-rsa');
-
-const fs = require('fs');
-
-const Encrypt = require('./Encrypt');
-const Decrypt = require('./Decrypt');
 
 // create app & server
 const app = express();
 const server = http.createServer(app);
 const socket = socketIO(server);
 
-let publicKey;
-let privateKey;
 let serverDH;
 
 function serveStaticFiles() {
@@ -49,12 +41,6 @@ function serveClientPage() {
   });
 }
 
-// Generate RSA keys for encryption
-function generateRSAKeyPair() {
-  publicKey = fs.readFileSync('public.pem', 'utf-8');
-  privateKey = fs.readFileSync('private.pem', 'utf-8');
-}
-
 function performKeyExchange(socket) {
   serverDH = crypto.createDiffieHellman(2048);
   serverDH.generateKeys();
@@ -63,89 +49,83 @@ function performKeyExchange(socket) {
   socket.on('client-public-key', (clientPublicKey) => {
     const sharedSecret = serverDH.computeSecret(clientPublicKey, 'hex', 'hex');
     // You can use the sharedSecret for encryption or derive keys from it.
+
+    const keyMaterial = crypto.createHash('sha256').update(sharedSecret, 'hex').digest();
+    
+    // Use derived keys for encryption or integrity
+    socket.encryptionKey = keyMaterial.slice(0, 16);  // For example, use the first 16 bytes as an encryption key
+    socket.integrityKey = keyMaterial.slice(16, 32); 
   });
 }
 
-// function encryptWithSharedSecret(sharedSecret, data) {
-//   const cipher = crypto.createCipheriv('aes-256-cbc', sharedSecret, Buffer.from('0123456789abcdef0'));
-//   return Buffer.concat([cipher.update(data, 'utf8'), cipher.final()]);
-// }
-
-// function decryptWithSharedSecret(sharedSecret, data) {
-//   const iv = Buffer.from('0123456789abcdef0', 'hex'); // Convert the IV from hex to a buffer
-//   const decipher = crypto.createDecipheriv('aes-256-cbc', sharedSecret, iv);
-//   return Buffer.concat([decipher.update(data, 'base64', 'utf8'), decipher.final()]);
-// }
-
-function exchangePublicKeys()
+function encryptUsingEncryptionKey(message)
 {
-  console.log('server public key sent: ', publicKey);
-  socket.emit('server-public-key', publicKey); 
-  socket.on('client-public-key', (clientPublicKey) => {
-    console.log('client public key receivrd: ', clientPublicKey);
-    return clientPublicKey;
+  // Encrypt the message using the derived encryption key
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(socket.encryptionKey), Buffer.alloc(16, 0));
+  const encryptedMessage = Buffer.concat([cipher.update(message, 'utf8'), cipher.final()]);
+  return encryptedMessage;
+}
+
+function decryptUsingEncryptionKey(message)
+{
+  // Decrypt the data using the derived encryption key
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(socket.encryptionKey), Buffer.alloc(16, 0));
+  const decryptedData = Buffer.concat([decipher.update(encryptedData, 'hex'), decipher.final()]);
+  return decryptedData;
+}
+
+function sendMessageToClient(message) 
+{
+  const encryptedMessage = encryptUsingEncryptionKey(message);
+
+  // Calculate HMAC for message integrity
+  const hmac = crypto.createHmac('sha256', socket.integrityKey).update(encryptedMessage).digest('hex');
+
+  // Send the encrypted message and HMAC to the client
+  socket.emit('server-message', encryptedMessage.toString('hex'), hmac);
+}
+
+function receiveMessageFromClient() 
+{
+  socket.on('client-message', (encryptedData, receivedHMAC) => {
+    
+    const decryptedData = decryptUsingEncryptionKey(encryptedData);
+    // Verify the integrity of the received message using HMAC
+    const computedHMAC = crypto.createHmac('sha256', socket.integrityKey).update(decryptedData).digest('hex');
+    
+    if (computedHMAC === receivedHMAC) {
+      console.log('Message integrity verified. Decrypted data:', decryptedData.toString());
+      // Process the decrypted and authenticated data
+    } else {
+      console.log('Message integrity check failed. Discarding message.');
+      // Handle the case where the message may have been tampered with
+    }
   });
 }
 
 
 function handleSocketConnection(socket) {
   console.log('A user connected');
-  clientPublicKey = exchangePublicKeys();
-  console.log('client public key receivrd: ', clientPublicKey);
-  // const encryptData = new Encrypt(clientPublicKey);
-  // const decryptData = new Decrypt(privateKey);
+  performKeyExchange();
 
-  const originalData = 'Hello, client!';
-  //const encryptedData = encryptData.encrypt(originalData);
-  console.log('Encrypted:', originalData);
-  socket.emit('server-message', originalData);
-  socket.on('client-message', (clientData) =>{
-    // const decryptedData = decryptData.decrypt(clientData);
-    console.log("Decrypted: ", clientData);
+  const msgToClient = 'Hello, client!';
+  sendMessageToClient(msgToClient);
+  receiveMessageFromClient();
+
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
-
-//   socket.on('exchange-keys', (data) => {
-//     const { dh, sharedSecret } = performKeyExchange(socket, Buffer.from(data.clientPublicKey, 'base64'));
-
-//     socket.on('client-message', (encryptedData) => {
-//       const decryptedData = decryptWithSharedSecret(sharedSecret, encryptedData);
-//       console.log('Received and decrypted data:', decryptedData.toString());
-
-//       const response = 'Message from Server to Client';
-//       const encryptedResponse = crypto.publicEncrypt({
-//         key: Buffer.from(data.clientPublicKey, 'base64'),
-//         padding: crypto.constants.RSA_PKCS1_PADDING,
-//       }, Buffer.from(response, 'utf8'));
-
-//       socket.emit('server-message', encryptedResponse.toString('base64'));
-//     });
-
-//     // Send a welcome message to the connected client
-//     socket.emit('message', 'Welcome to the chat!');
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
 }
 
 function startServer() {
   serveStaticFiles();
   serveClientPage();
-  generateRSAKeyPair();
 
   server.listen(PORT, LOCAL_IP, () => {
     console.log(`Server is running on http://${LOCAL_IP}:${PORT}`);
   });
 }
-// generateRSAKeyPair();
-// const encryptData = new Encrypt(publicKey);
-// const decryptData = new Decrypt(privateKey);
-// const originalData = 'Hello, World!';
-// const encryptedData = encryptData.encrypt(originalData);
-// console.log('Encrypted:', encryptedData);
 
-// const decryptedData = decryptData.decrypt(encryptedData);
-// console.log("Decrypted: ", decryptedData);
 startServer();
 socket.on('connection', handleSocketConnection);
