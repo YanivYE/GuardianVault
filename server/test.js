1a4060540a8d9918233a8d0c12765967a66d1f11
Elior Yousefi <elioryous@gmail.com>
2024-03-08 22:18:40 +0200

    Create test.js

---
+++ server/test.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+// Define IP address and port
+const IP_ADDRESS = '127.0.0.1'; // Change this to your desired IP address
+const PORT = 3000; // Change this to your desired port number
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  // Set response header
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // Write response content
+  res.write('<h1>Hello, World!</h1>');
+
+  // End the response
+  res.end();
+});
+
+// Start the server
+server.listen(PORT, IP_ADDRESS, () => {
+  console.log(`Server running at http://${IP_ADDRESS}:${PORT}/`);
+});
