f11bc89f147eeb79fec28e7c5c77d46ad67290a4
YanivYE <yanivye106@gmail.com>
2024-03-12 17:55:25 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,106 +12,63 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 // Serve static files
-function serveStaticFiles() {
-  const staticPaths = [
-    '../client/signup',
-    '../client/login',
-    '../client/forgot-password',
-    '../client/code-verification',
-    '../client/reset-password',
-    '../client/upload',
-    '../client/download',
-    '../client/menu',
-    '../client/index',
-    '../client'
-  ];
+const staticPaths = [
+    'signup',
+    'login',
+    'forgot-password',
+    'code-verification',
+    'reset-password',
+    'upload',
+    'download',
+    'menu',
+    'index',
+    ''
+];
 
-  staticPaths.forEach(staticPath => {
-    const fullPath = path.join(__dirname, staticPath);
-    app.use(express.static(fullPath));
-  });
-}
+staticPaths.forEach(staticPath => {
+    app.use(express.static(path.join(__dirname, `../client/${staticPath}`)));
+});
 
-function serveClientPage() {
-  app.get('/', (req, res) => {
-    const filePath = path.join(__dirname, '../client/index/index.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
+// Serve client pages
+const clientPages = [
+    { route: '/', file: 'index/index.html' },
+    { route: '/login', file: 'login/login.html' },
+    { route: '/signup', file: 'signup/signup.html' },
+    { route: '/upload', file: 'upload/upload.html' },
+    { route: '/download', file: 'download/download.html' },
+    { route: '/menu', file: 'menu/menu.html' },
+    { route: '/forgot-password', file: 'forgot-password/forgot-password.html' },
+    { route: '/code-verification', file: 'code-verification/code-verification.html' },
+    { route: '/reset-password', file: 'reset-password/reset-password.html' }
+];
 
-  app.get('/login', (req, res) => {
-    const filePath = path.join(__dirname, '../client/login/login.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-
-  app.get('/signup', (req, res) => {
-    const filePath = path.join(__dirname, '../client/signup/signup.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-
-  app.get('/upload', (req, res) => {
-    const filePath = path.join(__dirname, '../client/upload/upload.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-
-  app.get('/download', (req, res) => {
-    const filePath = path.join(__dirname, '../client/download/download.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-
-  app.get('/menu', (req, res) => {
-    const filePath = path.join(__dirname, '../client/menu/menu.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-
-  app.get('/forgot-password', (req, res) => {
-    const filePath = path.join(__dirname, '../client/forgot-password/forgot-password.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-
-  app.get('/code-verification', (req, res) => {
-    const filePath = path.join(__dirname, '../client/code-verification/code-verification.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-
-  app.get('/reset-password', (req, res) => {
-    const filePath = path.join(__dirname, '../client/reset-password/reset-password.html');
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(filePath);
-  });
-}
-
-function startServer() {  
-  serveStaticFiles();
-  serveClientPage();
-
-  server.on('error', (error) => {
-    console.error('Server error:', error.message);
-  });
-  
-  server.listen(config.PORT, config.LOCAL_IP, () => {
-    console.log(`Server is running on http://${config.LOCAL_IP}:${config.PORT}`);
-  });
-
-  io.on('connection', (socket) => {
-    const userId = Utils.generateUniqueUserId();
- 
-    console.log(`User ${userId} connected`);
-    const socketHandler = new Handler.SocketHandler(socket);
-
-    socketHandler.handleClientConnection();
-
-    socket.on('disconnect', () => {
-        console.log(`User ${userId} disconnected`);
+clientPages.forEach(({ route, file }) => {
+    app.get(route, (req, res) => {
+        const filePath = path.join(__dirname, `../client/${file}`);
+        res.setHeader('Content-Type', 'text/html');
+        res.sendFile(filePath);
     });
-  });
+});
+
+// Start server
+function startServer() {
+    server.on('error', (error) => {
+        console.error('Server error:', error.message);
+    });
+
+    server.listen(config.PORT, config.LOCAL_IP, () => {
+        console.log(`Server is running on http://${config.LOCAL_IP}:${config.PORT}`);
+    });
+
+    io.on('connection', (socket) => {
+        const userId = Utils.generateUniqueUserId();
+        console.log(`User ${userId} connected`);
+        const socketHandler = new Handler.SocketHandler(socket);
+        socketHandler.handleClientConnection();
+        socket.on('disconnect', () => {
+            console.log(`User ${userId} disconnected`);
+        });
+    });
 }
 
 startServer();
