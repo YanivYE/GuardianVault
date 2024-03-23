751ad957f0ed27c9f346df10cdaa3940c287a68e
YanivYE <yanivye106@gmail.com>
2024-03-23 17:46:58 +0200

    modified uplaod scripts

--- frontend/static/js/views/uploadView.js
+++ frontend/static/js/views/uploadView.js
@@ -65,28 +65,45 @@ export default class UploadView extends AbstractView{
     if (!window.client.logedIn) {
       window.client.navigateTo('/login'); // Redirect to login page if not logged in
     }
+
+    // Buttons and Form Elements
+    const publicButton = document.getElementById("publicButton");
+    const privateButton = document.getElementById("privateButton");
+    const uploadForm = document.getElementById("uploadForm");
+    const selectAllCheckbox = document.getElementById('selectAllUsers');
+    const fileInput = document.getElementById('fileInput-upload');
+    const fileUpload = document.getElementById("fileUpload");
+    const userSelectGroup = document.getElementById("userSelectGroup");
+    const message = document.getElementById("message"); // Error message element
+    const loader = document.getElementById('uploadLoader');
+
+    setupEventListeners();
+    getUsersListFromServer().then(users => generateUserCheckboxes(users));
+
+    // Setup Event Listeners
+    function setupEventListeners() {
+        // Add Event Listeners
+        publicButton.addEventListener("click", onPublicButtonClick);
+        privateButton.addEventListener("click", onPrivateButtonClick);
+        selectAllCheckbox.addEventListener('change', onSelectAllCheckboxChange);
+        fileInput.addEventListener('change', displayUploadedFile);
+        fileUpload.addEventListener('click', onFileUploadClick);
+        fileUpload.addEventListener('dragover', onFileUploadDragOver);
+        fileUpload.addEventListener('dragleave', onFileUploadDragLeave);
+        fileUpload.addEventListener('drop', onFileUploadDrop);
+    }
+
     
-    var publicButton = document.getElementById("publicButton");
-    var privateButton = document.getElementById("privateButton");
-    var userSelectGroup = document.getElementById("userSelectGroup");
-    var uploadForm = document.getElementById("uploadForm");
-    var message = document.getElementById("message"); // Error message element
-    var loader = document.getElementById('uploadLoader');
-    
-    // Hide the userSelectGroup field initially
-    userSelectGroup.style.display = "none";
-    
-    // Add event listener to public button
-    publicButton.addEventListener("click", function () {
+    // Event Listener Functions
+    function onPublicButtonClick() {
         // Show the userSelectGroup field when the public button is clicked
         userSelectGroup.style.display = "block";
         // Add "active" class to public button and remove it from private button
         publicButton.classList.add("active");
         privateButton.classList.remove("active");
-    });
-    
-    // Add event listener to private button
-    privateButton.addEventListener("click", function () {
+    }
+
+    function onPrivateButtonClick() {
         // Hide the userSelectGroup field when the private button is clicked
         userSelectGroup.style.display = "none";
         // Add "active" class to private button and remove it from public button
@@ -94,33 +111,92 @@ export default class UploadView extends AbstractView{
         publicButton.classList.remove("active");
         // Clear user selection if "Private" is chosen
         clearUserSelection();
-    });
-    
-    const selectAllCheckbox = document.getElementById('selectAllUsers');
-    selectAllCheckbox.addEventListener('change', function () {
+    }
+
+    function onSelectAllCheckboxChange() {
         const checkboxes = document.querySelectorAll('input[name="users"]');
         checkboxes.forEach(function (checkbox) {
             checkbox.checked = selectAllCheckbox.checked;
         });
-    });
-    
-    // Wrap the code fetching users in an async function
-    (async function () {
-        var users = await getUsersListFromServer();
-    
-        // Dynamically generate checkboxes for each user
-        var userCheckboxContainer = document.getElementById("userCheckboxContainer");
+    }
+
+    function displayUploadedFile() {
+        const uploadedFileContainer = document.querySelector('.file-upload-text');
+        if (fileInput.files.length > 0) {
+            uploadedFileContainer.innerText = fileInput.files[0].name;
+        } else {
+            uploadedFileContainer.innerText = 'Drag or select files';
+        }
+    }
+
+    function onFileUploadClick() {
+        fileInput.click();
+    }
+
+    function onFileUploadDragOver(event) {
+        event.preventDefault();
+        fileUpload.classList.add('drag-over');
+    }
+
+    function onFileUploadDragLeave() {
+        fileUpload.classList.remove('drag-over');
+    }
+
+    function onFileUploadDrop(event) {
+        event.preventDefault();
+        fileUpload.classList.remove('drag-over');
+        if (event.dataTransfer.files.length > 0) {
+            document.querySelector('.file-upload-text').innerText = event.dataTransfer.files[0].name;
+        }
+    }
+
+    // Other Functions
+    function clearUserSelection() {
+        const checkboxes = document.querySelectorAll('input[name="users"]');
+        checkboxes.forEach(function(checkbox) {
+            checkbox.checked = false;
+        });
+    }
+
+    function errorAlert(errorMessage) {
+        showMessage(errorMessage, "red");
+    }
+
+    function successAlert(successMessage) {
+        showMessage(successMessage, "green");
+    }
+
+    function showMessage(messageText, color) {
+        message.style.display = "block";
+        message.style.color = color;
+        message.innerText = messageText;
+    }
+
+    async function getUsersListFromServer() {
+        try {
+            const usersListRequest = 'UsersList$';
+            const usersListResult = await window.client.transferToServer(usersListRequest, 'usersListResult');
+
+            return usersListResult === "empty" ? [] : usersListResult.split(',');
+        } catch (error) {
+            console.error("Error getting users list from server:", error);
+            // Handle the error as needed
+        }
+    }
+
+    function generateUserCheckboxes(users) {
+        const userCheckboxContainer = document.getElementById("userCheckboxContainer");
         users.forEach(function (user) {
-            var checkboxDiv = document.createElement("div");
+            const checkboxDiv = document.createElement("div");
             checkboxDiv.classList.add("checkbox-item");
     
-            var checkbox = document.createElement("input");
+            const checkbox = document.createElement("input");
             checkbox.type = "checkbox";
             checkbox.name = "users";
             checkbox.value = user;
             checkbox.id = user;
     
-            var label = document.createElement("label");
+            const label = document.createElement("label");
             label.htmlFor = user;
             label.appendChild(document.createTextNode(user));
     
@@ -128,98 +204,6 @@ export default class UploadView extends AbstractView{
             checkboxDiv.appendChild(label);
             userCheckboxContainer.appendChild(checkboxDiv);
         });
-    })();
-
-    
-    // Add event listener for file selection
-    var fileInput = document.getElementById('fileInput-upload');
-    fileInput.addEventListener('change', displayUploadedFile);
-
-    // Function to display the uploaded file
-    function displayUploadedFile() {
-        const uploadedFileContainer = document.querySelector('.file-upload-text');
-        
-        // Check if a file is selected
-        if (fileInput.files.length > 0) {
-            const file = fileInput.files[0];
-            const fileName = file.name;
-            
-            // Display the file name in the file-upload-text span
-            uploadedFileContainer.innerText = fileName;
-        } else {
-            // If no file is selected, clear the container
-            uploadedFileContainer.innerText = 'Drag or select files';
-        }
-    }
-
-    // Add event listener for drag and drop functionality
-    var fileUpload = document.getElementById("fileUpload");
-    fileUpload.addEventListener('click', function() {
-        // Trigger click event on the input element
-        fileInput.click();
-    });
-
-    fileUpload.addEventListener('dragover', function(event) {
-        event.preventDefault();
-        fileUpload.classList.add('drag-over');
-    });
-
-    fileUpload.addEventListener('dragleave', function() {
-        fileUpload.classList.remove('drag-over');
-    });
-
-    fileUpload.addEventListener('drop', function(event) {
-        event.preventDefault();
-        fileUpload.classList.remove('drag-over');
-        
-        const fileList = event.dataTransfer.files;
-        // Display the first file name in the file-upload-text span
-        if (fileList.length > 0) {
-            const fileName = fileList[0].name;
-            document.querySelector('.file-upload-text').innerText = fileName;
-        }
-    });
-
-    // Function to clear user selection
-    function clearUserSelection() {
-        var checkboxes = document.querySelectorAll('input[name="users"]');
-        checkboxes.forEach(function(checkbox) {
-            checkbox.checked = false;
-        });
-    }
-
-    function errorAlert(errorMessage)
-    {
-        message.style.display = "block"; // Show error message
-        message.style.color = "red";
-        message.innerText = errorMessage;
-    }
-
-    function successAlert(successMessage)
-    {
-        message.style.display = "block";
-        message.style.color = "green";
-        message.innerText = successMessage;
-    }
-
-    async function getUsersListFromServer() {
-        try {
-
-            const usersListRequest = 'UsersList$';
-            const usersListResult = await window.client.transferToServer(usersListRequest, 'usersListResult');
-
-            if(usersListResult === "empty")
-            {
-                return [];
-            }
-            else{
-                const usersList = usersListResult.split(',');
-                return usersList;
-            }
-        } catch (error) {
-            console.error("Error getting users list from server:", error);
-            // Handle the error as needed
-        }
     }
     
     // Add event listener for form submission
@@ -227,62 +211,79 @@ export default class UploadView extends AbstractView{
     
         event.preventDefault(); // Prevent default form submission
     
-        // Validate inputs
-        var fileName = document.getElementById("fileName").value.trim();
-        var fileStatus = publicButton.classList.contains("active") || privateButton.classList.contains("active");
-        var users = []; // Initialize users as an empty array
-        var privateUpload = true;
-    
+        // Get form inputs
+        const fileName = document.getElementById("fileName").value.trim();
+        const fileStatus = publicButton.classList.contains("active") || privateButton.classList.contains("active");
+        let users = [];
+        let privateUpload = true;
+
+        // Determine users and upload status
         if (publicButton.classList.contains("active")) {
             privateUpload = false;
-            var checkedCheckboxes = document.querySelectorAll('input[name="users"]:checked');
+            const checkedCheckboxes = document.querySelectorAll('input[name="users"]:checked');
             users = Array.from(checkedCheckboxes).map(checkbox => checkbox.value);
         }
-    
-        var fileInput = document.getElementById('fileInput-upload');
-        var file = fileInput.files[0]; // Get the selected file
-    
-        // Check if all inputs are valid
-        if (fileName !== '' && fileStatus && (privateUpload || users.length > 0) && file) {
-            const fileExtension = file.name.split('.').pop().toLowerCase(); // Extract the file extension and convert it to lowercase
-    
-            // List of PHP file extensions
-            const phpExtensions = ['php', 'php3', 'php4', 'php5', 'phtml'];
-            const JSExtentiosns = ['js', 'mjs', 'jsx', 'ts', 'tsx'];
-            const executableExtentions = ['exe', 'bat', 'sh', 'cmd'];
-    
-            // Check if the file extension is in the list of PHP extensions
-            if (phpExtensions.includes(fileExtension) || JSExtentiosns.includes(fileExtension) || executableExtentions.includes(fileExtension)) {
-                errorAlert("Any PHP, JavaScript and executable \nfile types are not allowed!");
+
+        const fileInput = document.getElementById('fileInput-upload');
+        const file = fileInput.files[0]; // Get the selected file
+
+        const fileExtension = file.name.split('.').pop().toLowerCase();
+
+        // Validate form inputs
+        if (validateInputs(fileName, fileExtension, fileStatus, users, privateUpload, file)) {
+            const validationResult = await validateFileName(fileName + '.' + fileExtension, users);
+            if (validationResult === "Success") {
+                document.getElementById("uploadButton").disabled = true;
+                // Read file content and upload
+                readFileAndUpload(file);
             } else {
-                const filePath = fileName + '.' + fileExtension;
-                const validationResult = await validateFileName(filePath, users);
-                if (validationResult === "Success") {
-                    const fileSize = file.size;
-    
-                    if (fileSize > 1024 * 1024 * 100) // 100MB
-                    {
-                        errorAlert("File too large, limit is 100MB")
-                    } else {
-                        document.getElementById("uploadButton").disabled = true;
-                        // All inputs are valid, proceed with form submission
-                        const reader = new FileReader();
-    
-                        reader.onload = async (event) => {
-                            let fileContent = event.target.result;
-    
-                            await uploadFile(fileContent, fileSize);
-                        };
-                        reader.readAsDataURL(file);
-                    }
-                } else {
-                    errorAlert("File name is already taken");
-                }
+                errorAlert("File name is already taken");
             }
-        } else {
-            errorAlert("Please fill out all required fields first");
         }
     });
+
+    // Function to validate form inputs
+    function validateInputs(fileName, fileExtension, fileStatus, users, privateUpload, file) {
+        const phpExtensions = ['php', 'php3', 'php4', 'php5', 'phtml'];
+        const JSExtensions = ['js', 'mjs', 'jsx', 'ts', 'tsx'];
+        const executableExtensions = ['exe', 'bat', 'sh', 'cmd'];
+
+        if (fileName === '') {
+            errorAlert("Please enter a file name");
+            return false;
+        }
+        if (!fileStatus) {
+            errorAlert("Please select either public or private upload");
+            return false;
+        }
+        if (!privateUpload && users.length === 0) {
+            errorAlert("Please select users for public upload");
+            return false;
+        }
+        if (!file) {
+            errorAlert("Please select a file to upload");
+            return false;
+        }
+        if (phpExtensions.includes(fileExtension) || JSExtensions.includes(fileExtension) || executableExtensions.includes(fileExtension)) {
+            errorAlert("Any PHP, JavaScript, and executable\n file types are not allowed!");
+            return false;
+        }
+        if (file.size > 1024 * 1024 * 100) {
+            errorAlert("File too large, limit is 100MB");
+            return false;
+        }
+        return true;
+    }
+
+    // Function to read file content and upload
+    function readFileAndUpload(file) {
+        const reader = new FileReader();
+        reader.onload = async (event) => {
+            const fileContent = event.target.result;
+            await uploadFile(fileContent, file.size);
+        };
+        reader.readAsDataURL(file);
+    }
     
     async function uploadFile(fileContent, fileSize) {
         const blockSize = 1024 * 500; // 500KB chunk size
@@ -328,23 +329,14 @@ export default class UploadView extends AbstractView{
     }
     
     async function validateFileName(fileName, shareWithUsers) {
-    
         const validateFileNameRequest = 'validateName$' + fileName + '$' + shareWithUsers;
-        const validateNameResult = await window.client.transferToServer(validateFileNameRequest, 'validateNameResult');
-    
-        return validateNameResult
+        return await window.client.transferToServer(validateFileNameRequest, 'validateNameResult');
     }
     
     async function sendFileBlock(block, blockIndex, totalBlocks) {
-    
         const uploadFileBlockRequest = 'UploadFileBlock$' + blockIndex + '$' + block + '$' + totalBlocks;
-        const uploadBlockResult = await window.client.transferToServer(uploadFileBlockRequest, 'uploadBlockResult');
-    
-        return uploadBlockResult;
+        return await window.client.transferToServer(uploadFileBlockRequest, 'uploadBlockResult');
     }
-  
-  
-  
   
   }
 }
